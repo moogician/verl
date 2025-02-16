810f0717a287885096dc29dba5eb1560d3b59ae0
Hao Wang <hwang628@berkeley.edu>
2025-02-16 15:51:18 -0800

    update

---
+++ train_model.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# alias python='/home/tony/miniconda3/envs/sec-rl/bin/python'
+# alias python3='/home/tony/miniconda3/envs/sec-rl/bin/python3'
+# alias pip='/home/tony/miniconda3/envs/sec-rl/bin/pip'
+
+export N_GPUS=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+ray stop --force && ray start --head --include-dashboard=True
+export BASE_MODEL="models/DeepSeek-R1-Distill-Qwen-7B"
+export DATA_DIR="/home/tony/verl/dataset"
+export ROLLOUT_TP_SIZE=1
+export EXPERIMENT_NAME=cweval-r1-qwen-7b-grpo
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+#actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \ # down
+#actor_rollout_ref.actor.ppo_mini_batch_size=32 \ # down
+#actor_rollout_ref.actor.ppo_micro_batch_size=2 \ # down
+#actor_rollout_ref.ref.log_prob_micro_batch_size=1 \ # down
+/home/tony/miniconda3/envs/sec-rl/bin/python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=$DATA_DIR/train.parquet \
+    data.val_files=$DATA_DIR/test.parquet \
+    data.train_batch_size=32 \
+    data.val_batch_size=8 \
+    data.max_prompt_length=10000 \
+    data.max_response_length=30000 \
+    actor_rollout_ref.model.path=$BASE_MODEL \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=1 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['wandb'] \
+    +trainer.val_before_train=False \
+    trainer.default_hdfs_dir=null \
+    trainer.n_gpus_per_node=$N_GPUS \
+    trainer.nnodes=1 \
+    trainer.save_freq=10 \
+    trainer.test_freq=10 \
+    trainer.project_name=TinyZero \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.total_epochs=15 2>&1 | tee r1-qwen-7b-grpo.log
